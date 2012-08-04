77b9cbe2166c62251e1e84fd2e8aeb7b3b2721e4
Mark Burns <markthedeveloper@gmail.com>
2012-08-04 10:46:46 +0100

    new app with twilio config

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Roulette::Application.config.secret_token = 'ce4f9839f88a871d88ce5b1d90734d72bbb5b5917d86f1de43457e67c132bd6d310e3038ddc67e933a2e9f4ed06e73a9767496bd343fb96dd31b7ea3a90af6c4'
---
+++ config/initializers/twilio.rb
@@ -0,0 +1,5 @@
+Twilio::Config.setup \
+  :account_sid  => 'AC79e93279afd31f3aa71650b1659ff81d',
+  :auth_token   => 'b488db5d83575b7b69dc348ed9a9ffde'
+
+
