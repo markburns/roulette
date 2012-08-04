5a99127ec26fc65d20b181b419103001fed83d4f
Mark Burns <markthedeveloper@gmail.com>
2012-08-04 13:24:35 +0100

    check for valid bet

--- app/models/bet_parser.rb
+++ app/models/bet_parser.rb
@@ -5,5 +5,15 @@ class BetParser
     a, p = digits.split "*"
     @amount   = a.to_i
     @position = p.to_i
+    @valid = (@amount > 0) and valid_position?
+  end
+
+  def valid?
+    @valid
+  end
+
+  def valid_position?
+    (@position >= 0) and
+    (@position <=36)
   end
 end
--- spec/models/bet_parser_spec.rb
+++ spec/models/bet_parser_spec.rb
@@ -2,10 +2,12 @@ require './app/models/bet_parser'
 
 describe BetParser do
   {
-    "123*0" =>  {amount: 123, position: 0},
-    "123*33" => {amount: 123, position: 33},
-    "4*1"   =>  {amount: 4,   position: 1},
-    "123*2" =>  {amount: 123, position: 2}
+    "123*0"   => {amount: 123, position: 0,    valid: true},
+    "123*33"  => {amount: 123, position: 33,   valid: true},
+    "4*1"     => {amount: 4,   position: 1,    valid: true},
+    "123*2"   => {amount: 123, position: 2,    valid: true},
+    "123*122" => {amount: 123, position: 122,  valid: false},
+    "0*122"   => {amount: 0,   position: 122,  valid: false}
   }.each do |input, output|
     specify do
       @bet= BetParser.new input
